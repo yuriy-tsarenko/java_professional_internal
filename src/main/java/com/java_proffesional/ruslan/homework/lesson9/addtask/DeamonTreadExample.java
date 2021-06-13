401de11f21725dd6ab53f0e8efe4f1c3ee3dc67f
Ruslan Kucherenko <59806506+Svitliy@users.noreply.github.com>
2021-06-13 21:55:33 +0300

    HOMEWORK-9: addtask in the process of.

--- src/main/java/com/java_proffesional/ruslan/homework/lesson9/addtask/DeamonTreadExample.java
+++ src/main/java/com/java_proffesional/ruslan/homework/lesson9/addtask/DeamonTreadExample.java
@@ -1,35 +1,52 @@
 package com.java_proffesional.ruslan.homework.lesson9.addtask;
 
+import lombok.SneakyThrows;
+
 /**
- * @author
- *
+ * @author Ruslan
+ * Homework9 addtask
  * Создайте поток-демон и выведите про него как можно больше информации.
  */
-public class DeamonTreadExample {
 
+    public class DeamonTreadExample {
 
-        public static void main(String[] args) throws InterruptedException {
-
-            Thread t = new Thread(DeamonTreadExample::run);
-            t.setDaemon(true);
-            t.start();
-
-            Thread.sleep(10000);
-            System.out.println("main thread exited");
-        }
-
-    private static void run() {
-        int count = 0;
-        while (true) {
-            count++;
-            try {
-                System.out.println("inside try" + count);
-                Thread.sleep(1000);
-            } catch (InterruptedException e) {
-                e.printStackTrace();
-            } finally {
-                System.out.println("finally executed" + count);
-            }
-        }
+    @SneakyThrows
+    public static void main(String[] args) {
+         new JobThread(true).start();    //set it to true and false and run twice.
+            Thread.sleep(7500);
+        System.out.println("Main Thread ending");
     }
 }
+class JobThread extends Thread {
+    private boolean isDeamon;
+    JobThread(boolean isDeamon) {
+        // When false, (i.e. when it a user thread),
+        // the Worker thread continues to run.
+        // When true, (i.e. when it a daemon thread),
+        // the Worker thread terminates when the main
+        // thread terminates.
+        this.isDeamon = isDeamon;
+        setDaemon(isDeamon);
+    }
+
+    @SneakyThrows
+    public void run() {
+        System.out.println("I am a " + (isDeamon ? "Deamon Thread" : "User Thread (none-deamon)"));
+        int counter = 0;
+        while (counter < 10) {
+            counter++;
+            System.out.println("\tworking from Worker thread " + counter++);
+            String nameThread =  getName(); // gets the thread name
+            long idThread = getId();// returns the thread identifier
+            boolean alive =  isAlive(); // the execution state of the thread
+            int priority = getPriority();//gets the thread priority
+            System.out.printf("имя потока: %s; идентификатор потока: %d; поток выполняется: %s; приоритет потока: %d;%n",
+                    nameThread, idThread, alive, priority);
+                sleep(5000);
+        }
+        System.out.println("\tWorker thread ends. ");
+    }
+}
+
+
+
