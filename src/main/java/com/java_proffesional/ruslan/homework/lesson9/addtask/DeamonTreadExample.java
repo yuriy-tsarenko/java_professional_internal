093235b115c1609e019690629f3038553bf2142c
Ruslan Kucherenko <59806506+Svitliy@users.noreply.github.com>
2021-06-16 22:56:31 +0300

    HOMEWORK-9: task all done.

--- src/main/java/com/java_proffesional/ruslan/homework/lesson9/addtask/DeamonTreadExample.java
+++ src/main/java/com/java_proffesional/ruslan/homework/lesson9/addtask/DeamonTreadExample.java
@@ -8,17 +8,19 @@ import lombok.SneakyThrows;
  * Создайте поток-демон и выведите про него как можно больше информации.
  */
 
-    public class DeamonTreadExample {
+public class DeamonTreadExample {
 
     @SneakyThrows
     public static void main(String[] args) {
-         new JobThread(true).start();    //set it to true and false and run twice.
-            Thread.sleep(7500);
+        new JobThread(true).start();    //set it to true and false and run twice.
+        Thread.sleep(7500);
         System.out.println("Main Thread ending");
     }
 }
+
 class JobThread extends Thread {
     private boolean isDeamon;
+
     JobThread(boolean isDeamon) {
         // When false, (i.e. when it a user thread),
         // the Worker thread continues to run.
@@ -36,13 +38,16 @@ class JobThread extends Thread {
         while (counter < 10) {
             counter++;
             System.out.println("\tworking from Worker thread " + counter++);
-            String nameThread =  getName(); // gets the thread name
+            String nameThread = getName(); // gets the thread name
             long idThread = getId();// returns the thread identifier
-            boolean alive =  isAlive(); // the execution state of the thread
+            boolean alive = isAlive(); // the execution state of the thread
             int priority = getPriority();//gets the thread priority
-            System.out.printf("имя потока: %s; идентификатор потока: %d; поток выполняется: %s; приоритет потока: %d;%n",
+            System.out.printf("имя потока: %s; "
+                            + "идентификатор потока: %d; "
+                            + "поток выполняется: %s; "
+                            + "приоритет потока: %d;%n",
                     nameThread, idThread, alive, priority);
-                sleep(5000);
+            sleep(5000);
         }
         System.out.println("\tWorker thread ends. ");
     }
